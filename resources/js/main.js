efef065943022e10880e6a618008cf79269ac572
MCodes96 <mikayil_gacek@outlook.com>
2022-09-21 12:17:18 +0200

    finish v1 of show more/less button in about section - now working without any animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,6 +4,17 @@
 const bioExtended = document.querySelector('.bio-extended');
 const showBtn = document.querySelector('.show-btn');
 
-showBtn.addEventListener('click', function () {
+showBtn.addEventListener('click', (e) => {
   bioExtended.classList.toggle('hide');
+  if (!bioExtended.classList.contains('hide')) {
+    showBtn.innerHTML =
+      'Show less <img src="./resources/images/arrow-down.svg"></img></a>';
+    showBtn.lastChild.style.transform = 'rotate(180deg)';
+    bioExtended.style.height = 'auto';
+  } else {
+    showBtn.innerHTML =
+      'Show more <img src="./resources/images/arrow-down.svg"></img></a>';
+    showBtn.lastChild.style.transform = 'rotate(0deg)';
+    bioExtended.style.height = '0';
+  }
 });
