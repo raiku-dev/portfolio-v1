08e8993b91f602af098aa3c6afa255f9f10bd371
Mikayil Gacek <mikayil_gacek@icloud.com>
2023-07-30 21:38:28 +0200

    fixes

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@
 
 const splash = document.querySelector('.splash');
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     splash.classList.add('display-none');
   }, 2000);
