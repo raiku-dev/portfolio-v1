951526670888334a3d414564adb4aeaad11e104b
Mikayil Gacek <mikayil_gacek@icloud.com>
2024-08-13 21:24:03 +0200

    update portfolio

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,3 @@
-// INTRO SCREEN
-
-const splash = document.querySelector(".splash");
-
-document.addEventListener("DOMContentLoaded", () => {
-	setTimeout(() => {
-		splash.classList.add("display-none");
-	}, 2000);
-	setTimeout(() => {
-		splash.style.display = "none";
-	}, 4000);
-});
-
 // MOBILE NAV
 
 const mobileNav = document.querySelector(".mobile-nav");
@@ -37,14 +24,16 @@ document.querySelectorAll(".nav-link").forEach((link) =>
 const bioExtended = document.querySelector(".bio-extended");
 const showBtn = document.querySelector(".show-btn");
 
-showBtn.addEventListener("click", (e) => {
+showBtn.addEventListener("click", () => {
 	bioExtended.classList.toggle("hide");
 	if (!bioExtended.classList.contains("hide")) {
-		showBtn.innerHTML = 'Show less <img src="./resources/images/arrow-down.svg"></img></a>';
+		showBtn.innerHTML =
+			'Show less <img src="./resources/images/arrow-down.svg"></img></a>';
 		showBtn.lastChild.style.transform = "rotate(180deg)";
 		bioExtended.style.height = "auto";
 	} else {
-		showBtn.innerHTML = 'Show more <img src="./resources/images/arrow-down.svg"></img></a>';
+		showBtn.innerHTML =
+			'Show my life story (basically) <img src="./resources/images/arrow-down.svg"></img></a>';
 		showBtn.lastChild.style.transform = "rotate(0deg)";
 		bioExtended.style.height = "0";
 	}
