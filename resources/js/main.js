65ae8a98107ac1ced3501df36cea26a7e9c4a608
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-06-05 19:16:18 +0200

    fix some of the BS I did, when I didn't know any better...

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,53 +1,57 @@
-// MOBILE NAV
+document.addEventListener('DOMContentLoaded', () => {
+	// MOBILE NAV
 
-const mobileNav = document.querySelector('.mobile-nav');
-const navMenu = document.querySelector('.nav-menu');
-const mainElement = document.getElementById('main');
+	const mobileNav = document.querySelector('.mobile-nav');
+	const navMenu = document.querySelector('.nav-menu');
+	const mainElement = document.getElementById('main');
 
-mobileNav.addEventListener('click', () => {
-	mobileNav.classList.toggle('active');
-	navMenu.classList.toggle('active');
-	mainElement.classList.toggle('blur');
-});
+	mobileNav.addEventListener('click', () => {
+		mobileNav.classList.toggle('active');
+		navMenu.classList.toggle('active');
+		mainElement.classList.toggle('blur');
+		document.body.classList.toggle('mobile-nav-open');
+	});
 
-document.querySelectorAll('.nav-link').forEach((link) =>
-	link.addEventListener('click', () => {
-		mobileNav.classList.remove('active');
-		navMenu.classList.remove('active');
-		mainElement.classList.remove('blur');
-	})
-);
+	document.querySelectorAll('.nav-link').forEach((link) =>
+		link.addEventListener('click', () => {
+			mobileNav.classList.remove('active');
+			navMenu.classList.remove('active');
+			mainElement.classList.remove('blur');
+			document.body.classList.remove('mobile-nav-open');
+		})
+	);
 
-// ABOUT-SECTION
+	// ABOUT-SECTION
 
-// Show more button in about-text
-const bioExtended = document.querySelector('.bio-extended');
-const showBtn = document.querySelector('.show-btn');
+	// Show more button in about-text
+	const bioExtended = document.querySelector('.bio-extended');
+	const showBtn = document.querySelector('.show-btn');
 
-showBtn.addEventListener('click', () => {
-	bioExtended.classList.toggle('hide');
-	if (!bioExtended.classList.contains('hide')) {
-		showBtn.innerHTML =
-			'Show less <img src="./resources/images/arrow-down.svg"></img></a>';
-		showBtn.lastChild.style.transform = 'rotate(180deg)';
-		bioExtended.style.height = 'auto';
-	} else {
-		showBtn.innerHTML =
-			'Show my life story (basically) <img src="./resources/images/arrow-down.svg"></img></a>';
-		showBtn.lastChild.style.transform = 'rotate(0deg)';
-		bioExtended.style.height = '0';
-	}
-});
-
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('visible');
+	showBtn.addEventListener('click', () => {
+		bioExtended.classList.toggle('hide');
+		if (!bioExtended.classList.contains('hide')) {
+			showBtn.innerHTML =
+				'Show less <img src="./resources/images/arrow-down.svg"></img></a>';
+			showBtn.lastChild.style.transform = 'rotate(180deg)';
+			bioExtended.style.height = 'auto';
 		} else {
-			// entry.target.classList.remove('visible');
+			showBtn.innerHTML =
+				'Show my life story (basically) <img src="./resources/images/arrow-down.svg"></img></a>';
+			showBtn.lastChild.style.transform = 'rotate(0deg)';
+			bioExtended.style.height = '0';
 		}
 	});
-});
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible');
+			} else {
+				// entry.target.classList.remove('visible');
+			}
+		});
+	});
+
+	const hiddenElements = document.querySelectorAll('.hidden');
+	hiddenElements.forEach((el) => observer.observe(el));
+});
